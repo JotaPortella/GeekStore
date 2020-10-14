fa7b47993eb469d34aa17d23bf8dec6d605f355b
Jota Portella <Jota Portella@192.168.0.9>
2020-10-14 01:10:53 -0300

    Adicionando Projeto Geek Store

---
+++ src/test/java/br/com/geeksotre/geekstore/GeekstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geeksotre.geekstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
